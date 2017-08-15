a1523bfe1590fe4148b40ee136925e729777ef1f
Dmitriy Doluk <dmitriy.doluk@gmail.com>
2017-08-15 18:44:30 +0300

    10.Saving Users

---
+++ src/Yoda/UserBundle/DataFixtures/ORM/LoadUsers.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Yoda\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Yoda\UserBundle\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+
+class LoadUsers implements FixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+
+    private $container;
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('darth');
+        $user->setPassword($this->encodePassword($user, 'darthpass'));
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    private function encodePassword(User $user, $plainPassword)
+    {
+        $encoder = $this->container->get('security.encoder_factory')
+            ->getEncoder($user);
+
+        return $encoder->encodePassword($plainPassword, $user->getSalt());
+    }
+}
