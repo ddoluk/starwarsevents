59efa5e473f46f8d2577222141f36e8e5339f22b
Dmitriy Doluk <dmitriy.doluk@gmail.com>
2017-08-24 14:45:23 +0300

    07.OneToMany: The Inverse Side of a Relationship

--- play.php
+++ play.php
@@ -5,10 +5,10 @@ use Symfony\Component\Debug\Debug;
 
 umask(0000);
 
-$loader = require_once __DIR__.'/app/bootstrap.php.cache';
+$loader = require_once __DIR__ . '/app/bootstrap.php.cache';
 Debug::enable();
 
-require_once __DIR__.'/app/AppKernel.php';
+require_once __DIR__ . '/app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
@@ -21,14 +21,14 @@ $container->set('request', $request);
 
 // all our setup is done!!!!!!
 
-use Yoda\EventBundle\Entity\Event;
+use Doctrine\ORM\EntityManager;
 
-$event = new Event();
-$event->setName('Darth\'s surprise birthday party');
-$event->setLocation('Deathstar');
-$event->setTime(new \DateTime('tomorrow noon'));
-//$event->setDetails('Ha! Darth HATES surprises!!!!');
-
+/** @var EntityManager $em */
 $em = $container->get('doctrine')->getManager();
-$em->persist($event);
-$em->flush();
+
+$wayne = $em->getRepository('UserBundle:User')
+    ->findOneByUsernameOrEmail('wayne');
+
+foreach ($wayne->getEvents() as $event) {
+    var_dump($event->getName());
+}
