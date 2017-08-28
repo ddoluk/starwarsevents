e2d22b410c1901c9b98b04d52de5d78102bc3e03
Dmitriy Doluk <dmitriy.doluk@gmail.com>
2017-08-28 15:46:25 +0300

    21.Your Very First Service

--- src/Yoda/EventBundle/Controller/ReportController.php
+++ src/Yoda/EventBundle/Controller/ReportController.php
@@ -5,6 +5,7 @@ namespace Yoda\EventBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Yoda\EventBundle\Reporting\EventReportManager;
 
 class ReportController extends Controller
 {
@@ -15,23 +16,10 @@ class ReportController extends Controller
     public function updatedEventsAction()
     {
         $em = $this->getDoctrine()->getManager();
+        $eventRepositoryManager = new EventReportManager($em);
 
-        $events = $em->getRepository('EventBundle:Event')
-            ->getRecentlyUpdatedEvents();
+        $content = $eventRepositoryManager->getRecentlyUpdatedReport();
 
-        $rows = array();
-
-        foreach ($events as $event) {
-            $data = array(
-                $event->getId(),
-                $event->getName(),
-                $event->getTime()->format('Y-m-d H:i:s'),
-            );
-
-            $rows[] = implode(', ', $data);
-        }
-
-        $content = implode('\n', $rows);
         $response = new Response($content);
         $response->headers->set('Content-Type', 'text/csv');
 
