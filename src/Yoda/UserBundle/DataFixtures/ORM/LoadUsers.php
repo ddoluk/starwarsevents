1d7813a7558872602745eecf8c7da20496dc6f8f
Dmitriy Doluk <dmitriy.doluk@gmail.com>
2017-08-29 19:23:18 +0300

    27.Doctrine Event Listeners

--- src/Yoda/UserBundle/DataFixtures/ORM/LoadUsers.php
+++ src/Yoda/UserBundle/DataFixtures/ORM/LoadUsers.php
@@ -22,13 +22,13 @@ class LoadUsers implements FixtureInterface, ContainerAwareInterface, OrderedFix
         $user = new User();
         $user->setUsername('darth');
         $user->setEmail('darth@deathstar.com');
-        $user->setPassword($this->encodePassword($user, 'darthpass'));
+        $user->setPlainPassword('darthpass');
         $manager->persist($user);
 
         $admin = new User();
         $admin->setUsername('wayne');
         $admin->setEmail('wayne@deathstar.com');
-        $admin->setPassword($this->encodePassword($admin, 'waynepass'));
+        $admin->setPlainPassword('waynepass');
         $admin->setRoles(array('ROLE_ADMIN'));
         $manager->persist($admin);
             
@@ -39,14 +39,6 @@ class LoadUsers implements FixtureInterface, ContainerAwareInterface, OrderedFix
     public function setContainer(ContainerInterface $container = null)
     {
         $this->container = $container;
-    }
-
-    private function encodePassword(User $user, $plainPassword)
-    {
-        $encoder = $this->container->get('security.encoder_factory')
-            ->getEncoder($user);
-
-        return $encoder->encodePassword($plainPassword, $user->getSalt());
     }
 
     function getOrder()
